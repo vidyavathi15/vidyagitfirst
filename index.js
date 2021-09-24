afc1775b9a25bdab49c9962c93f3aef3a258f9e0
Lukkigithub15@ <vidyavathi15@gmail.com>
2021-09-24 04:20:47 +0000

    ratio and factorial

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (request, response) => {
-  response.send("Hello World!");
+app.get("/date", (request, response) => {
+  let date = new Date();
+  response.send(`Today's date is ${date}`);
 });
 
-app.listen(5000);
+app.listen(3000);
